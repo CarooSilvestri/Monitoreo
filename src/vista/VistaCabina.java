e1892552bbee6435f1949ecc8daba80178e86074
Caro <msilvestri@fi.uba.ar>
2018-12-12 16:55:09 -0300

    añadiendo

---
+++ src/vista/VistaCabina.java
@@ -0,0 +1,20 @@
+package vista;
+
+import javafx.scene.layout.VBox;
+
+public class VistaCabina extends VBox {
+
+    private int pisos;
+
+    public VistaCabina(int cantPisos) {
+
+        this.pisos = cantPisos;
+    }
+
+    private void cabinas() {
+
+    }
+
+    private void dibujarCabina() {}
+
+}
--- src/vista/VistaMenu.java
+++ src/vista/VistaMenu.java
@@ -11,18 +11,21 @@ public class VistaMenu extends HBox {
 
     private void barraMenu() {
 
+
         Button btn = new Button("Configuración del sistema");
         Button btn1 = new Button("Anulacion llamadas");
         Button btn2 = new Button("Alarma de incendio");
         btn2.setStyle("-fx-background-color: red;" +
                 "-fx-text-fill: white;" +
                 "-fx-font-weight: bold;");
-
+        Button btnSalir = new Button("Salir");
+        HBox subh = new HBox(btn, btn1, btn2);
         super.setSpacing(20);
         super.setStyle("-fx-background-color:  #336699;");
         super.setPadding(new Insets(5,0,5,15));
         super.setAlignment(Pos.CENTER_LEFT);
-        super.getChildren().addAll(btn, btn1, btn2);
+
+        super.getChildren().addAll(subh, btnSalir);
     }
 
     public void dibujarMenu() {
